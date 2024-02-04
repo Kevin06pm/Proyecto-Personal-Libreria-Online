b65f7b368901521bf0ace8dd9ccb6b271a5a0392
unknown <snapaspue@gmail.com>
2024-02-04 01:15:08 +0100

    Migraciones de junior y seniors con su clave foranea

---
+++ app/Http/Controllers/JuniorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Junior;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class JuniorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Junior $junior)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Junior $junior)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Junior $junior)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Junior $junior)
+    {
+        //
+    }
+}
