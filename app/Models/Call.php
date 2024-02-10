a79e4de7dfc9b1a66af592447d60e892f1c34ff9
GianfrancoCampos <camposacevedo23@iessantiagohernandez.com>
2024-02-10 20:05:55 +0100

    Creacion de Fillables

--- app/Models/Call.php
+++ app/Models/Call.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Call extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['numero_telefono','fecha_llamada','hora_llamada','numero_linea_interno'];
 }
