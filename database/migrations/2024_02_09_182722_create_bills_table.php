7fea0e7c2f2df64b54a72d65752eac0ef4044d55
Kevin06pm <keviiin06pm@gmail.com>
2024-02-10 19:08:44 +0100

    cambio

--- database/migrations/2024_02_09_182722_create_bills_table.php
+++ database/migrations/2024_02_09_182722_create_bills_table.php
@@ -11,19 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-<<<<<<< HEAD:database/migrations/2024_02_09_174019_add_fk_to_item_bills_table.php
         Schema::table('item_bills', function (Blueprint $table) {
             $table->bigInteger('num_factura')->unsigned()->after('id');
             $table->foreign('num_factura')->references('id')->on('calls')->onUpdate('cascade')->onDelete('cascade');
 
             $table->bigInteger('cod_producto')->unsigned()->after('num_factura');
             $table->foreign('cod_producto')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
-=======
-        Schema::create('bills', function (Blueprint $table) {
-            $table->id();
-            $table->integer('numero_factura')->unique();
-            $table->timestamps();
->>>>>>> d775a1be347e3210f7e498cb742837832c03fee3:database/migrations/2024_02_09_182722_create_bills_table.php
         });
     }
 
